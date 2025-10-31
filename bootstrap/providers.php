6f530ed5e2018cf078ce9c2f0993e578f7b91b52
Jonas Silva <jonas.silva502@gmail.com>
2025-10-31 20:39:22 -0300

    feat(dependencias): adiciona swagger, filament, spatie e faker

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,8 @@
 <?php
-declare(strict_types=1);
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 ];
